6f64624fd2eda8b15aa9b00630113e722c23b344
DylanN341 <56797787+DylanN341@users.noreply.github.com>
2020-02-01 12:49:21 -0800

    Added some class stuff

--- 2020Robot/src/main/include/spinner.h
+++ 2020Robot/src/main/include/spinner.h
@@ -1,1 +1,17 @@
 #include "ctre/Phoenix.h"
+
+class PHHSSpinner
+{
+
+    private:
+
+    const int kSpinnerMotorID = 1;
+
+    const float kSpinnerHoriz = 1.0;
+
+    public:
+    
+    void spinnerOn(float speed);
+    
+
+};
