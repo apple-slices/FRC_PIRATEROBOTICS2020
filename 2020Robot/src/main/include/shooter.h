adc94e776d4b04220210d76395b2770ce5769998
BIG-TOFU <56799087+BIG-TOFU@users.noreply.github.com>
2020-02-05 20:42:31 -0800

    Nothing

--- 2020Robot/src/main/include/shooter.h
+++ 2020Robot/src/main/include/shooter.h
@@ -1,22 +1,4 @@
-#include "ctre/Phoenix.h"
+#pragma once 
 
-class PHHSShooter
-{
-    private:
-    constexpr static int kShooterMotorID = 1;
 
-    WPI_TalonSRX m_shooterMotor{kShooterMotorID};
-    
-    public:
-    
-    constexpr static float kShooterMotor = 1.0;
-    constexpr static float kShooterMotorOff = 0.0;
 
-    constexpr static int kShooterMotorOnBtn = 1; //Button A on Xbox 360
-    constexpr static int kShooterMotorOffBtn = 2; //Button B on Xbox 360
-
-    void shooterOn(float speed);
-    
-    void shooterOff(float speed);
-
-};
